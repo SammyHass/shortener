32fb17079f940246ba1d3fa21cd141450d2ad950
Sammy Hass <me@sammyhass.io>
2019-07-12 22:09:01 +0100

    Added ability to redirect user to new url

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func findRecord(q string) (url string, err error) {
+	lines, err := os.Open("data.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer lines.Close()
+	r := csv.NewReader(lines)
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		if record[0] == q {
+			return record[1], nil
+		}
+	}
+	return "", nil
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	code := r.URL.Path[1:]
+	uri, err := findRecord(code)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(code)
+	http.Redirect(w, r, uri, 301)
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
